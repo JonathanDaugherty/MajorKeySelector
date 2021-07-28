225038447dd02a631b1b1f04be29a6fc2a31e3a4
jmdaugherty94@gmail.com <jmdaugherty94@gmail.com>
2021-07-28 00:44:22 -0400

    Add project files.

---
+++ MajorKeySelector/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MajorKeySelector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] keys = { "A", "B", "C", "D", "E", "F", "G", "Bb", "Db", "Eb", "Gb", "Ab" };
+
+            Random r = new Random();
+
+            int index = r.Next(keys.Length);
+
+            Console.WriteLine($"Your scale today is in the key of: {keys[index]}");
+        }
+    }
+}
